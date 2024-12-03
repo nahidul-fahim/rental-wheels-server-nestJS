d1078b93664230cf5b4fbbb16ee9616ead4e479f
nahidul-fahim <nahidul7562@gmail.com>
2024-12-03 16:56:09 +0600

    added mongodb module and some necessary featured in main file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,16 @@ import { AppService } from './app.service';
 import { CarModule } from './car/car.module';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from '@/config';
+import { MongoDbModule } from './mongo-db/mongo-db.module';
 
 @Module({
   imports: [
     CarModule,
+    MongoDbModule,
     ConfigModule.forRoot({
       validate,
       isGlobal: true
-    })
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  const configService = app.get(ConfigService);
+  app.enableCors();
+  app.setGlobalPrefix('api/v1');
+
+  const port = configService.get<number>('PORT') ?? 3001;
+
+  await app.listen(port, async () => {
+    console.log(`=======Server running on port ${port}=======`);
+  });
 }
 bootstrap();
