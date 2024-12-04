16f61876073909c1f666d277b9ef64a21181045a
nahidul-fahim <nahidul7562@gmail.com>
2024-12-04 10:36:23 +0600

    Added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.enableCors();
   app.setGlobalPrefix('api/v1');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Rental Wheels Server')
+    .setDescription('The Rental Wheels Server API description')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/v1', app, documentFactory);
 
   const port = configService.get<number>('PORT') ?? 3001;
 
